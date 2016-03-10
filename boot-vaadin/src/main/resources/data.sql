000d48134baccee0f78e981d38ae847dc067f902
msahal <sahal.mohamed.enis@gmail.com>
2016-03-10 22:59:41 +0100

    comments

--- boot-vaadin/src/main/resources/data.sql
+++ boot-vaadin/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 --insert into customer(first_name,last_name) values('Joonas','Lehtinen');
-insert into customer(first_name,last_name) values('Petter','Holmström');
+--insert into customer(first_name,last_name) values('Petter','Holmström');
 insert into customer(first_name,last_name) values('Josh','Long');
